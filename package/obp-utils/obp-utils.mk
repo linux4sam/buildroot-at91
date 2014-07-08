b34d57bfecf186e373fbb2f8f03aea0f43e5e98e
Nicolas Ferre <nicolas.ferre@atmel.com>
2014-07-08 14:47:42 +0200

    obp-utils: move temporarly to alternate github repository
    As there are some fixes to add to current mainline, move to this temporary
    github repository that handles these fixes.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- package/obp-utils/obp-utils.mk
+++ package/obp-utils/obp-utils.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-OBP_UTILS_VERSION = dc87d231a46586ed8f726cb41c6ed21570c298e5
-OBP_UTILS_SITE = git://github.com/atmel-maxtouch/obp-utils.git
+OBP_UTILS_VERSION = 4cdc89ab4d7b73e1c7b670e1ab832c8ad639f5d8
+OBP_UTILS_SITE = git://github.com/noglitch/obp-utils.git
 
 OBP_UTILS_AUTORECONF = YES
+OBP_UTILS_CONF_OPT += --disable-manpages
+
 
 $(eval $(autotools-package))
