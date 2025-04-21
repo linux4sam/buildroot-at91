5249f87b8b36d591870bcf1aafb023069899823b
Nayab Sayed <nayabbasha.sayed@microchip.com>
2025-04-21 14:26:01 +0530

    package/optee-test: bump to 4.5.0
    Update optee-test to the latest tagged release
    
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    [Julien: move package archive hash to the "Locally computed" section]
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    Reviewed-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>

--- package/optee-test/optee-test.mk
+++ package/optee-test/optee-test.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPTEE_TEST_VERSION = 4.4.0
+OPTEE_TEST_VERSION = 4.5.0
 OPTEE_TEST_LICENSE = GPL-2.0, BSD-2-Clause,
 OPTEE_TEST_LICENSE_FILES = LICENSE.md
 
